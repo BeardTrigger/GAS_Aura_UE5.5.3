633dd7d5be265a8cc37d036c2c7fceb55fc5343c
BeardTrigger <karel.terra@gmail.com>
2025-02-24 22:33:03 -0500

    Player and Spear Goblin Enemy BPs

---
+++ Source/Aura/Private/Character/CharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Virtualis Terra
+
+
+#include "Character/CharacterBase.h"
+
+ACharacterBase::ACharacterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();	
+}
---
+++ Source/Aura/Public/Character/EnemyBase.h
@@ -0,0 +1,17 @@
+// Copyright Virtualis Terra
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "EnemyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyBase : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
