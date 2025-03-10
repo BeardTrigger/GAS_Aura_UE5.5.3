390d9081f329a09c44d1f50a66c1ed128e219940
BeardTrigger <karel.terra@gmail.com>
2025-03-10 10:07:31 -0400

    Starting on UI. Added UserWidget base class that inherits from UUserWidget and a WidgetController that is a UObject. Set up some bluepring callable fucntion to set the controller on the Widgets. Added variables to the Controller to hold the Ability System, Player Controller, Player State and Attribute set so it can get data from them to broadcast to the Widget

---
+++ Source/Aura/Private/UI/Controller/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Virtualis Terra
+
+
+#include "UI/Controller/AuraWidgetController.h"
+
---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Virtualis Terra
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
