0cbac098a15f9fa957655827a22aa005b27ff1e6
BeardTrigger <karel.terra@gmail.com>
2025-03-18 23:16:15 -0400

    Added infinite fire area effect that ticks health down over time. Reworked how the GEs are applied by added an application and removal policy that can be set in the BPs. Added support for adding and removing infinite effects based on overlap callbacks

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -27,6 +27,69 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	GameplayEffectContextHandle.AddSourceObject(this);
 	const FGameplayEffectSpecHandle GameplayEffectSpecHandle = ASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, GameplayEffectContextHandle);
 	ASC->ApplyGameplayEffectSpecToSelf(*GameplayEffectSpecHandle.Data.Get());
+}
+
+void AAuraEffectActor::RemoveEffectsFromTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (ASC == nullptr) return;
 	
+	check(GameplayEffectClass);
+	
+	ASC->RemoveActiveGameplayEffectBySourceEffect(GameplayEffectClass, nullptr);
+}
+
+
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+
+	if (InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+		InfiniteTargetActor.Add(TargetActor);
+	}
+
+	if (bDestroyOnEffectApplication)
+	{
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+
+	if (InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		// Remove Infinite Effect here
+		if (InfiniteTargetActor.Contains(TargetActor))
+		{
+			const int TopActorIndex = InfiniteTargetActor.Find(TargetActor);
+			RemoveEffectsFromTarget(InfiniteTargetActor[TopActorIndex], InfiniteGameplayEffectClass);
+			InfiniteTargetActor.RemoveAt(TopActorIndex);
+		}		
+		
+		if (bDestroyOnEffectRemoval)
+		{
+			Destroy();
+		}
+	}
 }
 
