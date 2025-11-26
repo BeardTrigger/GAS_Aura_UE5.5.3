1821577420e47394352efee856aedf9659d62d75
BeardTriggerLaptop <kchabera@gmail.com>
2025-11-26 07:28:57 -0500

    Looping over gameplay tags in GamePlayEffect callback. Printing Asset Tags to the screen with a debug message

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,5 +11,13 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 
 void UAuraAbilitySystemComponent::GameplayEffectAppliedCallback(UAbilitySystemComponent* ASCPointer, const FGameplayEffectSpec& GESpec, FActiveGameplayEffectHandle ActiveGEHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 2.f, FColor::Blue, FString::Printf(TEXT("Effect Applied!")));
+	FGameplayTagContainer TagContainer;
+	GESpec.GetAllAssetTags(TagContainer);
+	
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		// Broadcast the tag to the widget controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Blue, Msg);
+	}
 }
